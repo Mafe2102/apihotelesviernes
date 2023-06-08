38fd3ee51d0adfed27689ddd5da1d99f2becda12
chenaoh <mfvilladaqu@gmail.com>
2023-06-08 10:00:58 -0500

    test

--- database/conexion.js
+++ database/conexion.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 export async function establecerConexion(){
     try{
-        await mongoose.connect(process.env.DATABASE)
+        await mongoose.connect("mongodb+srv://MariaVillada:Admin123@cluster0.1f3udbm.mongodb.net/test")
         console.log("Conexion exitosa")
     }catch(error){
         console.log("Conexion fallo"+error)
